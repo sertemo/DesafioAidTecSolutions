e9290de9951cc94b80299ec52e7327c0149651e3
sertemo <tejedor.moreno@gmail.com>
2024-04-23 22:45:26 +0200

    Creado script con argumentos para crear el dataset para modeling. falta dar opcion de guardarlo. Inicializado Sphinx para documentacion

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Aidtec Solutions\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D\x1b[DDesafio Kopuru \x1b[3\x1b[3~\x1b[3~ \x1b AidTec Solutions'
+copyright = '2024, Sergio Tejedor Moreno'
+author = 'Sergio Tejedor Moreno'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'es'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
